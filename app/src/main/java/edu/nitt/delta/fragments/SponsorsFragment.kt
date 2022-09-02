c00a3051528c5e14f7a936be8feb4e111f6cd6df
sarvesh-03 <83568494+sarvesh-03@users.noreply.github.com>
2022-09-02 19:30:06 +0530

    fix(layout): update nav graph (#36)

---
+++ app/src/main/java/edu/nitt/delta/fragments/SponsorsFragment.kt
@@ -0,0 +1,20 @@
+package edu.nitt.delta.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import edu.nitt.delta.R
+
+class SponsorsFragment : Fragment() {
+
+    override fun onCreateView(
+      inflater: LayoutInflater,
+      container: ViewGroup?,
+      savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sponsors, container, false)
+    }
+}
