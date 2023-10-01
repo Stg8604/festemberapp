517fd016684b6926c6202350991f24745d6e1255
S Selvanayagam <97583793+S-Selvanayagam@users.noreply.github.com>
2023-10-01 22:28:23 +0530

    fix(FCM): Add permission dialog and UI fix (#103)

--- app/src/main/java/edu/nitt/deltaapp/MainActivity.kt
+++ app/src/main/java/edu/nitt/deltaapp/MainActivity.kt
@@ -1,7 +1,14 @@
 package edu.nitt.deltaapp
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.messaging.FirebaseMessaging
@@ -13,10 +20,13 @@ import edu.nitt.deltaapp.helpers.viewLifecycle
 class MainActivity : AppCompatActivity() {
 
   private val binding by viewLifecycle(ActivityMainBinding::inflate)
+  private val postNotificationCode = 123
 
+  @RequiresApi(Build.VERSION_CODES.TIRAMISU)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(binding.root)
+    checkAndRequestPostNotificationsPermission()
   }
 
   override fun onStart() {
@@ -39,5 +49,39 @@ class MainActivity : AppCompatActivity() {
       Observer {
         showSnackbar_red("OOPS!! $it")
       })
+  }
+
+  @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+  private fun checkAndRequestPostNotificationsPermission() {
+    val permission = Manifest.permission.POST_NOTIFICATIONS
+
+    if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+      if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+        showPermissionRationaleDialog()
+      } else {
+        ActivityCompat.requestPermissions(
+          this,
+          arrayOf(permission),
+          postNotificationCode
+        )
+      }
+    }
+  }
+
+  private fun showPermissionRationaleDialog() {
+    AlertDialog.Builder(this)
+      .setTitle("Permission Required")
+      .setMessage("Please grant Notifications permission to receive notifications.")
+      .setPositiveButton("OK") { _, _ ->
+        ActivityCompat.requestPermissions(
+          this,
+          arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+          postNotificationCode
+        )
+      }
+      .setNegativeButton("Cancel") { _, _ ->
+        showSnackbar_red("Please grant Notifications permission to receive notifications.")
+      }
+      .show()
   }
 }
